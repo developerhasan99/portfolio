502a0fbcc44c70da647db380cca4587c93cf6490
webhasan99 <developerhasan99@gmail.com>
2021-10-19 18:47:22 +0600

    Portfolio site with Bootstrap framework!

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Google map scripts
+// const mapElement = document.getElementById('map-element');
+
+// function initMap() {
+
+//     const location = {
+//         lat: 24.363589,
+//         lng: 88.624135,
+//     }   
+
+//     const map = new google.maps.Map(mapElement, {
+//         zoom : 9,
+//         center: location,
+//     })
+
+//     const marker = new google.maps.Marker({
+//         position: location,
+//         map: map,
+//       });
+    
+// }
